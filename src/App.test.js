d716c1377330b93d8aedf5694b3b484119e77276
MedJawad <mohamedjawad.aatafay@gmail.com>
2020-08-26 15:44:37 +0100

    starting to implement enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
 import App from "./App";
-
+Enzyme.configure({ adapter: new EnzymeAdapter() });
 test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  // const linkElement = getByText(/header/i);
-  // expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
 });
